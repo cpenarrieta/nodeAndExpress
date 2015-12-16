99901d61d1f050c864ae63c2b36aebdeb3e5244c
Cristian Penarrieta <cristian.penarrieta@gmail.com>
2015-12-15 20:16:22 -0800

    add env variables

--- server/expressApp.js
+++ server/expressApp.js
@@ -33,5 +33,5 @@ var server = app.listen(process.env.PORT || 5000, function(){
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('listening at http://%s:%s', host, port);
+  console.log('listening at http://%s:%s', host, port, 'env var is',(process.env.TIMES || 5));
 });
--- server/spec/LiveServerIntegrationSpec.js
+++ server/spec/LiveServerIntegrationSpec.js
@@ -56,7 +56,6 @@ describe('server', function() {
     };
 
     request(requestParams, function(error, response, body) {
-      // Now if we request the log, that message we posted should be there:
       request('http://127.0.0.1:3000/classes/messages', function(error, response, body) {
           var messages = JSON.parse(body).results;
           expect(messages[0].username).to.equal('Jono');
